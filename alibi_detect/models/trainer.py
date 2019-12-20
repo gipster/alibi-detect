bd19f62933f70fdc55714627f5af7c4a62d94a9b
giovac73 <giova.physics@gmail.com>
2019-12-20 12:47:51 +0000

    Added logs saving best model on acc

--- alibi_detect/models/trainer.py
+++ alibi_detect/models/trainer.py
@@ -218,6 +218,7 @@ def trainer(model: tf.keras.Model,
                 else:
                     max_acc = max(test_accs)
                 if acc > max_acc:
+                    print('Accuracy improved from {} to {}. Saving model in {}'.format(max_acc, acc, best_model_path))
                     model.save_weights(best_model_path)
 
     if log_dir is not None:
